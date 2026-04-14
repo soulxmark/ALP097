41863bde1b974ba856727c4b63f207e8cf83a679
soulxmark <mark2899estimada@gmail.com>
2026-04-14 13:35:48 +0800

    chages

---
+++ data/original/admin.sql
@@ -0,0 +1,5 @@
+INSERT INTO users_tbl1 (uid, username, email, password_us, role, created_at, updated_at) 
+VALUES (1, 'admin', 'mark2899estimada@gmail.com', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', 'admin', NOW(), NOW())
+
+
+--for admin replacement
